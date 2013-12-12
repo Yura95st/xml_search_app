361f467fdd065871bf602a2c5d70fed5e0706b83
Yura95st <yura95st@gmail.com>
2013-12-12 22:26:23 +0200

    implemented LinqToXmlParser

--- xml_search_app/xml_search_app/XmlParsers/LinqToXmlParser.cs
+++ xml_search_app/xml_search_app/XmlParsers/LinqToXmlParser.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+
 using xml_search_app.Libs;
 using xml_search_app.Models;
 
@@ -19,7 +22,40 @@ namespace xml_search_app.XmlParsers
 
         public List<BookItem> ParseFile()
         {
-            throw new NotImplementedException();
+            XDocument xdoc = XDocument.Load(_file);
+            List<BookItem> bookItemList = new List<BookItem>();
+
+            try
+            {
+                var items = from item in xdoc.Descendants("item")
+                            select new
+                             {
+                                 FirstName = item.Element("name").Element("first_name").Value,
+                                 LastName = item.Element("name").Element("last_name").Value,
+                                 MiddleName = item.Element("name").Element("middle_name").Value,
+                                 City = item.Element("address").Element("city").Value,
+                                 Street = item.Element("address").Element("street").Value,
+                                 House = item.Element("address").Element("house").Value,
+                                 Apartment = item.Element("address").Element("apartment").Value,
+                                 PhoneNumber = item.Element("phone_number").Value,
+                             };
+
+                Address address;
+                Name name;
+                foreach (var item in items)
+                {
+                    name = new Name(item.FirstName, item.LastName, item.MiddleName);
+                    address = new Address(item.City, item.Street, item.House, Convert.ToInt32(item.Apartment));
+
+                    bookItemList.Add(new BookItem(name, address, Convert.ToInt32(item.PhoneNumber)));
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return bookItemList;
         }
 
         public List<BookItem> SearchInFile(string query, int type)
